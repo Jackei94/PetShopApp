1183a0ae5001f6b12ac5b8ec435fdbc1c656c586
Jackei94 <55141365+Jackei94@users.noreply.github.com>
2020-10-01 15:27:50 +0200

    Revert "Revert "Revert "Initial Commit"""
    This reverts commit 2098aa954e43952e93ffcc2f4d579fffa928c471.

---
+++ CustomerApp.Core/DomainService/IPetRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using PetShopApp.Core.Entity;
+
+namespace PetShopApp.Core.DomainService
+{
+    public interface IPetRepository
+    {
+        //PetRepository Interface
+        //Create Data
+        Pet Create(Pet pet);
+        //Read Data
+        Pet ReadById(int id);
+        IEnumerable<Pet> ReadAll();
+        //Update Data
+        Pet Update(Pet petUpdate);
+        //Delete Data
+        Pet Delete(int id);
+    }
+}
